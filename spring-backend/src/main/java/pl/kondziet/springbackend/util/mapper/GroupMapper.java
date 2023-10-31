734ae12455736bfc378c1e2e1399a2e2e3295f3d
kondziet <konrad.dabrowski54@gmail.com>
2023-10-31 17:47:38 +0100

    Create MapStruct GroupMapper

--- spring-backend/src/main/java/pl/kondziet/springbackend/util/mapper/GroupMapper.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/util/mapper/GroupMapper.java
@@ -1,6 +1,7 @@
 package pl.kondziet.springbackend.util.mapper;
 
 import org.mapstruct.Mapper;
+import pl.kondziet.springbackend.model.dto.GroupRequest;
 import pl.kondziet.springbackend.model.dto.GroupResponse;
 import pl.kondziet.springbackend.model.entity.Group;
 
@@ -8,4 +9,5 @@ import pl.kondziet.springbackend.model.entity.Group;
 public interface GroupMapper {
 
     GroupResponse groupToDto(Group group);
+    Group dtoToGroup(GroupRequest groupRequest);
 }
