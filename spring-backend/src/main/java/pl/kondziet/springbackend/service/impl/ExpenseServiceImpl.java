2f3941d3e8e15282a99d6b8428faac1d419eb76b
kondziet <konrad.dabrowski54@gmail.com>
2023-11-02 17:48:53 +0100

    Revert "Implement creating new PersonalExpense"
    This reverts commit 9cc51412dff342310c88edbb6ae82f0d1fea2e73.

--- spring-backend/src/main/java/pl/kondziet/springbackend/service/impl/ExpenseServiceImpl.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/service/impl/ExpenseServiceImpl.java
@@ -3,13 +3,11 @@ package pl.kondziet.springbackend.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import pl.kondziet.springbackend.model.dto.ExpenseDebtorRequest;
 import pl.kondziet.springbackend.model.dto.GroupExpenseRequest;
 import pl.kondziet.springbackend.model.dto.PersonalExpenseRequest;
 import pl.kondziet.springbackend.model.entity.*;
 import pl.kondziet.springbackend.repository.ExpenseDebtorRepository;
 import pl.kondziet.springbackend.repository.ExpenseRepository;
-import pl.kondziet.springbackend.repository.GroupRepository;
 import pl.kondziet.springbackend.repository.UserRepository;
 import pl.kondziet.springbackend.service.ExpenseService;
 import pl.kondziet.springbackend.util.mapper.ExpenseMapper;
@@ -24,70 +22,47 @@ public class ExpenseServiceImpl implements ExpenseService {
 
     private final ExpenseRepository expenseRepository;
     private final ExpenseDebtorRepository expenseDebtorRepository;
-    private final GroupRepository groupRepository;
     private final UserRepository userRepository;
     private final ExpenseMapper expenseMapper;
 
-    @Transactional
     @Override
-    public GroupExpense createGroupExpense(GroupExpenseRequest expenseDetails, User payer, UUID groupId) {
-
-        User mergedUser = userRepository.save(payer);
-        Group group = groupRepository.findById(groupId).orElseThrow();
-
-        GroupExpense expense = GroupExpense.builder()
-                .name(expenseDetails.name())
-                .payer(mergedUser)
-                .group(group)
-                .build();
-
-        GroupExpense savedExpense = expenseRepository.save(expense);
-
-        List<ExpenseDebtor> savedDebts = saveDebtsToExpense(savedExpense, expenseDetails.debts());
-
-        savedExpense.getExpenseDebtors().addAll(savedDebts);
-
-        return savedExpense;
+    public GroupExpense createGroupExpense(GroupExpenseRequest groupExpenseRequest, User payer, UUID groupId) {
+        return null;
     }
 
     @Transactional
     @Override
-    public PersonalExpense createPersonalExpense(PersonalExpenseRequest expenseDetails, User payer) {
+    public PersonalExpense createPersonalExpense(PersonalExpenseRequest personalExpenseRequest, User payer) {
 
         User mergedUser = userRepository.save(payer);
 
         PersonalExpense expense = PersonalExpense.builder()
-                .name(expenseDetails.name())
+                .name(personalExpenseRequest.name())
                 .payer(mergedUser)
                 .build();
 
         PersonalExpense savedExpense = expenseRepository.save(expense);
 
-        List<ExpenseDebtor> savedDebts = saveDebtsToExpense(savedExpense, expenseDetails.debts());
-
-        savedExpense.getExpenseDebtors().addAll(savedDebts);
-
-        return savedExpense;
-    }
-
-    @Transactional
-    private List<ExpenseDebtor> saveDebtsToExpense(Expense targetExpense, List<ExpenseDebtorRequest> debts) {
-        List<ExpenseDebtor> expenseDebts = debts.stream()
+        List<ExpenseDebtor> expenseDebtors = personalExpenseRequest.debts()
+                .stream()
                 .map(debtor -> {
                     User user = userRepository.findById(debtor.debtorId()).orElseThrow();
                     String currency = debtor.currency();
                     BigDecimal amount = debtor.amount();
 
                     return ExpenseDebtor.builder()
-                            .id(new ExpenseDebtor.ExpenseDebtorId(targetExpense.getId(), user.getId()))
+                            .id(new ExpenseDebtor.ExpenseDebtorId(savedExpense.getId(), user.getId()))
                             .debtor(user)
-                            .expense(targetExpense)
+                            .expense(savedExpense)
                             .currency(currency)
                             .amount(amount)
                             .build();
                 })
                 .toList();
 
-        return expenseDebtorRepository.saveAll(expenseDebts);
+        savedExpense.getExpenseDebtors().addAll(expenseDebtors);
+        expenseDebtorRepository.saveAll(expenseDebtors);
+
+        return savedExpense;
     }
 }
