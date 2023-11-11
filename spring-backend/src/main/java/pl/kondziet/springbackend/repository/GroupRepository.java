9601407fbc401a68698a9a1db08ad2048bfecd04
kondziet <konrad.dabrowski54@gmail.com>
2023-11-11 15:07:59 +0100

    Remove obsolete classes

--- spring-backend/src/main/java/pl/kondziet/springbackend/repository/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.kondziet.springbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.kondziet.springbackend.model.entity.Group;
-import java.util.UUID;
-
-public interface GroupRepository extends JpaRepository<Group, UUID> {
-}
