14bc92391e893b6627fe9fc96ece6170c062ed28
kondziet <konrad.dabrowski54@gmail.com>
2023-11-18 10:52:16 +0100

    Change field naming

--- spring-backend/src/main/java/pl/kondziet/springbackend/application/port/in/command/CreateGroupExpenseCommand.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/application/port/in/command/CreateGroupExpenseCommand.java
@@ -1,11 +1,12 @@
 package pl.kondziet.springbackend.application.port.in.command;
 
 import lombok.Builder;
+import pl.kondziet.springbackend.adapter.in.web.dto.DebtRequest;
 import pl.kondziet.springbackend.application.domain.model.id.GroupId;
 import pl.kondziet.springbackend.application.domain.model.id.UserId;
 
 import java.util.Set;
 
 @Builder
-public record CreateGroupExpenseCommand(String expenseName, UserId expensePayer, Set<DebtDetail> expenseDebtDetails, GroupId expenseGroupId) {
+public record CreateGroupExpenseCommand(String expenseName, UserId expensePayer, Set<DebtRequest> expenseDebts, GroupId expenseGroupId) {
 }
--- spring-backend/src/main/java/pl/kondziet/springbackend/application/port/in/command/CreatePersonalExpenseCommand.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/application/port/in/command/CreatePersonalExpenseCommand.java
@@ -1,10 +1,11 @@
 package pl.kondziet.springbackend.application.port.in.command;
 
 import lombok.Builder;
+import pl.kondziet.springbackend.adapter.in.web.dto.DebtRequest;
 import pl.kondziet.springbackend.application.domain.model.id.UserId;
 
 import java.util.Set;
 
 @Builder
-public record CreatePersonalExpenseCommand(String expenseName, UserId expensePayer, Set<DebtDetail> expenseDebtDetails) {
+public record CreatePersonalExpenseCommand(String expenseName, UserId expensePayer, Set<DebtRequest> expenseDebts) {
 }
