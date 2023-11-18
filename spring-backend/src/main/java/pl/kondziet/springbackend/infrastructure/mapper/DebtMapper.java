633625f6dd91202407639f7200f96ab698e0dfa8
kondziet <konrad.dabrowski54@gmail.com>
2023-11-18 10:50:08 +0100

    Remove mapping to DebtDetails

--- spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/mapper/DebtMapper.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/mapper/DebtMapper.java
@@ -4,10 +4,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import pl.kondziet.springbackend.adapter.in.web.dto.DebtRequest;
-import pl.kondziet.springbackend.adapter.out.persistence.entity.DebtJpaEntity;
 import pl.kondziet.springbackend.application.domain.model.entity.Debt;
 import pl.kondziet.springbackend.application.domain.model.id.UserId;
-import pl.kondziet.springbackend.application.port.in.command.DebtDetail;
 
 import java.util.Set;
 import java.util.UUID;
@@ -15,12 +13,9 @@ import java.util.UUID;
 @Mapper(componentModel = "spring")
 public interface DebtMapper {
 
-    DebtJpaEntity debtToDebtJpaEntity(Debt debt);
-    Set<Debt> debtDetailsToDebts(Set<DebtDetail> debtDetails);
-    Debt debtDetailToDebt(DebtDetail debtDetail);
-    Set<DebtDetail> debtRequestsToDebtDetails(Set<DebtRequest> debtRequests);
-    @Mapping(target = "debtorId", source = "debtorId", qualifiedByName = "idToUserId")
-    DebtDetail debtRequestToDebtDetail(DebtRequest debtRequest);
+    Set<Debt> debtRequestsToDebts(Set<DebtRequest> debtRequests);
+    @Mapping(target = "debtorId", source = "debtRequest.debtorId", qualifiedByName = "idToUserId")
+    Debt debtRequestToDebt(DebtRequest debtRequest);
     @Named("idToUserId")
     default UserId idToUserId(UUID id) {
         return new UserId(id);
