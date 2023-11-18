7d7197291c3db622ecea31eca704d82a35964e37
kondziet <konrad.dabrowski54@gmail.com>
2023-11-18 10:50:33 +0100

    Split use cases to separate classes

--- spring-backend/src/main/java/pl/kondziet/springbackend/application/port/in/GenerateTokenUseCase.java
+++
@@ -1,9 +0,0 @@
-package pl.kondziet.springbackend.application.port.in;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface GenerateTokenUseCase {
-
-    public String generateAccessToken(UserDetails userDetails);
-    public String generateRefreshToken(UserDetails userDetails);
-}
