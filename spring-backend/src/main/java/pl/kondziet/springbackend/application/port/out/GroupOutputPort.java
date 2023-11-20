effeff7bc043b94454094fd155ad7eaf03d49b53
kondziet <konrad.dabrowski54@gmail.com>
2023-11-20 15:19:15 +0100

    Remove unnecessary method

--- spring-backend/src/main/java/pl/kondziet/springbackend/application/port/out/GroupOutputPort.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/application/port/out/GroupOutputPort.java
@@ -1,9 +1,7 @@
 package pl.kondziet.springbackend.application.port.out;
 
 import pl.kondziet.springbackend.application.domain.model.entity.Group;
-import pl.kondziet.springbackend.application.domain.model.entity.GroupMembership;
 
 public interface GroupOutputPort {
     Group saveGroup(Group group);
-    void saveGroupMembership(GroupMembership groupMembership);
 }
