03f63e2dfc579ab9648f520cff52903cd0173106
kondziet <konrad.dabrowski54@gmail.com>
2023-12-24 14:12:37 +0100

    Add exchange rate caching to improve performance

--- spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/api/ExchangeRateFacade.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/api/ExchangeRateFacade.java
@@ -6,11 +6,19 @@ import org.springframework.web.client.RestClient;
 import pl.kondziet.springbackend.application.service.dto.ExchangeRateResponse;
 import pl.kondziet.springbackend.domain.model.valueobjects.ExchangeRate;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @Component
 public class ExchangeRateFacade {
 
     private final RestClient restClient;
     private final String apiKey;
+    private final Map<Pair<String, String>, Pair<ExchangeRate, LocalDateTime>> exchangeRateCache = new HashMap<>();
+    private final Duration cacheDuration = Duration.ofMinutes(1);
 
     public ExchangeRateFacade(@Value("${exchange-rate.api-url}") String apiUrl,
                               @Value("${exchange-rate.api-key}") String apiKey) {
@@ -22,18 +30,38 @@ public class ExchangeRateFacade {
     }
 
     public ExchangeRate loadExchangeRate(String baseCurrency, String targetCurrency) {
+        Pair<String, String> currencyPair = Pair.of(baseCurrency, targetCurrency);
+        Pair<ExchangeRate, LocalDateTime> cached = getExchangeRateFromCache(currencyPair);
+        if (Objects.isNull(cached) || isExpired(cached.getRight())) {
+            System.out.println("API");
+            ExchangeRate exchangeRate = getExchangeRateFromApi(currencyPair);
+            exchangeRateCache.put(currencyPair, Pair.of(exchangeRate, LocalDateTime.now()));
+            return exchangeRate;
+        } else {
+            return cached.getLeft();
+        }
+    }
+
+    private ExchangeRate getExchangeRateFromApi(Pair<String, String> currencyPair) {
         ExchangeRateResponse response = restClient
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/latest")
                         .queryParam("apikey", apiKey)
-                        .queryParam("base_currency", baseCurrency)
-                        .queryParam("currencies", targetCurrency)
+                        .queryParam("base_currency", currencyPair.getLeft())
+                        .queryParam("currencies", currencyPair.getRight())
                         .build()
                 )
                 .retrieve()
                 .body(ExchangeRateResponse.class);
 
-        return new ExchangeRate(baseCurrency, targetCurrency, response.data().get(targetCurrency));
+        return new ExchangeRate(currencyPair.getLeft(), currencyPair.getRight(), response.data().get(currencyPair.getRight()));
+    }
+
+    private Pair<ExchangeRate, LocalDateTime> getExchangeRateFromCache(Pair<String, String> currencyPair) {
+        return exchangeRateCache.get(currencyPair);
+    }
+    private boolean isExpired(LocalDateTime fetchTime) {
+        return fetchTime.plus(cacheDuration).isBefore(LocalDateTime.now());
     }
 }
--- spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/api/Pair.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/api/Pair.java
@@ -1,4 +1,39 @@
 package pl.kondziet.springbackend.infrastructure.api;
 
-public record CurrencyPair(String baseCurrency, String targetCurrency) {
+import java.util.Objects;
+
+public class Pair<L, R> {
+
+    private final L left;
+    private final R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public static <L, R> Pair<L, R> of(L left, R right) {
+        return new Pair<>(left, right);
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(left, pair.left) && Objects.equals(right, pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
 }
