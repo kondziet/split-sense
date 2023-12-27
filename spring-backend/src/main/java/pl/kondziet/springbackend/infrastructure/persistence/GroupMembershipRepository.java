6384b0248264834d84a8869c9759e819e0df9858
kondziet <konrad.dabrowski54@gmail.com>
2023-12-27 15:27:16 +0100

    Fix entity id type in JpaRepository

--- spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/persistence/GroupMembershipRepository.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/persistence/GroupMembershipRepository.java
@@ -3,7 +3,5 @@ package pl.kondziet.springbackend.infrastructure.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kondziet.springbackend.domain.model.entity.GroupMembership;
 
-import java.util.UUID;
-
-public interface GroupMembershipRepository extends JpaRepository<GroupMembership, UUID> {
+public interface GroupMembershipRepository extends JpaRepository<GroupMembership, GroupMembership.UserGroupId> {
 }
