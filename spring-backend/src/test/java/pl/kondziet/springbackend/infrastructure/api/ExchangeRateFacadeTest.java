cc1b93ca749ffe4bce1e793882e8e09a5ad54db3
kondziet <konrad.dabrowski54@gmail.com>
2023-12-29 11:48:58 +0100

    Create test for caching exchange rates

--- spring-backend/src/test/java/pl/kondziet/springbackend/infrastructure/api/ExchangeRateFacadeTest.java
+++ spring-backend/src/test/java/pl/kondziet/springbackend/infrastructure/api/ExchangeRateFacadeTest.java
@@ -2,46 +2,95 @@ package pl.kondziet.springbackend.infrastructure.api;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import pl.kondziet.springbackend.domain.model.valueobjects.ExchangeRate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ExchangeRateFacadeTest {
 
+    @Value("${exchange-rate.api-key}")
+    private String apiKey;
     @Autowired
     private ExchangeRateFacade exchangeRateFacade;
+    private static WireMockServer wireMockServer;
+
+    @BeforeAll
+    static void beforeAll() {
+        wireMockServer = new WireMockServer(8081);
+        wireMockServer.start();
+        WireMock.configureFor("localhost", 8081);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        wireMockServer.stop();
+    }
 
     @Test
     public void testLoadExchangeRate() {
-        WireMockServer wireMockServer = new WireMockServer(8081); // Start a WireMock server on port 8081
-        wireMockServer.start();
 
-        WireMock.configureFor("localhost", 8081); // Configure the WireMock client to interact with the server
-
-        String apiKey = "123";
         String baseCurrency = "USD";
         String targetCurrency = "PLN";
 
-        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/api/v1/latest"))
-                .withQueryParam("apikey", WireMock.equalTo(apiKey))
-                .withQueryParam("base_currency", WireMock.equalTo(baseCurrency))
-                .withQueryParam("currencies", WireMock.equalTo(targetCurrency))
-                .willReturn(WireMock.aResponse()
+        stubFor(get(urlPathEqualTo("/api/v1/latest"))
+                .withQueryParam("apikey", equalTo(apiKey))
+                .withQueryParam("base_currency", equalTo(baseCurrency))
+                .withQueryParam("currencies", equalTo(targetCurrency))
+                .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody("{\"data\": {\"" + targetCurrency + "\": \"3.88\"}}")));
+                        .withBody("""
+                                {
+                                    "data": {
+                                        "PLN": 3.8
+                                    }
+                                }
+                                """)));
 
         ExchangeRate exchangeRate = exchangeRateFacade.loadExchangeRate("USD", "PLN");
 
-        wireMockServer.stop();
+        assertThat(exchangeRate.rate()).isEqualTo(new BigDecimal("3.8"));
+        assertThat(exchangeRate.baseCurrency()).isEqualTo(baseCurrency);
+        assertThat(exchangeRate.targetCurrency()).isEqualTo(targetCurrency);
+    }
 
-        System.out.println(exchangeRate);
+    @Test
+    public void testLoadExchangeRateFromCache() {
 
+        String baseCurrency = "USD";
+        String targetCurrency = "PLN";
+
+        stubFor(get(urlPathEqualTo("/api/v1/latest"))
+                .withQueryParam("apikey", equalTo(apiKey))
+                .withQueryParam("base_currency", equalTo(baseCurrency))
+                .withQueryParam("currencies", equalTo(targetCurrency))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("""
+                                {
+                                    "data": {
+                                        "PLN": 3.8
+                                    }
+                                }
+                                """)));
+
+        ExchangeRate exchangeRate = exchangeRateFacade.loadExchangeRate("USD", "PLN");
+        ExchangeRate exchangeRateFromCache = exchangeRateFacade.loadExchangeRate("USD", "PLN");
+
+        verify(1, getRequestedFor(urlPathEqualTo("/api/v1/latest")));
+        assertThat(exchangeRate).isEqualTo(exchangeRateFromCache);
     }
 }
